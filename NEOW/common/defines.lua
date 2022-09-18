07e2ca893ab7a2c6419315abe3505aa0d9b81b1e
Niall490 <niallfarrell490@gmail.com>
2022-09-18 18:05:33 +0100

    moved main menu camera

--- NEOW/common/defines.lua
+++ NEOW/common/defines.lua
@@ -1721,18 +1721,18 @@ NAI =
 
 NFrontend =
 {
-	CAMERA_LOOKAT_X = 790.0, 			-- Rotation point in main menu
+	CAMERA_LOOKAT_X = 1350.0, 			-- Rotation point in main menu
 	CAMERA_LOOKAT_Y = 0.0,
-	CAMERA_LOOKAT_Z = 1000.0,
+	CAMERA_LOOKAT_Z = 800.0,
 	CAMERA_LOOKAT_SETTINGS_X = 1400.0,  -- Rotation point in settings
 	CAMERA_LOOKAT_SETTINGS_Y = 0.0,		-- Y is height
 	CAMERA_LOOKAT_SETTINGS_Z = 648.0,
-	CAMERA_START_X = 790.0,				-- Initial position in main menu
+	CAMERA_START_X = 1000.0,				-- Initial position in main menu
 	CAMERA_START_Y = 150.0,				-- Y is height
-	CAMERA_START_Z = 500.0,
-	CAMERA_END_X = 790.0,				-- Move to position in main menu
-	CAMERA_END_Y = 500.0,
-	CAMERA_END_Z = 600.0,
+	CAMERA_START_Z = 400.0,
+	CAMERA_END_X = 1350.0,				-- Move to position in main menu
+	CAMERA_END_Y = 1400.0,
+	CAMERA_END_Z = 200.0,
 	TIME_FROZEN = 1.0,  				-- Time before initial animation starts (some delay here so it should NOT be 0, then the animation starts before you can see it)
 	GUI_MOVE_SPEED = 800,				-- How fast sliding gui objects move ( pixels/s )
 	MAX_ZOOM_LEVEL = 2500,				-- How far out the player can zoom. Warning: At very high zoom levels, the ground will no longer render
@@ -1743,12 +1743,12 @@ NFrontend =
 
 	CAMERA_SPEED_IN_MENUS = 0.1,
 
-	FRONTEND_POS_X = 790.0,
-	FRONTEND_POS_Y = 500.0,
-	FRONTEND_POS_Z = 600.0,
-	FRONTEND_LOOK_X = 790.0,
+	FRONTEND_POS_X = 1350.0,
+	FRONTEND_POS_Y = 1400.0,
+	FRONTEND_POS_Z = 200.0,
+	FRONTEND_LOOK_X = 1350.0,
 	FRONTEND_LOOK_Y = 0.0,
-	FRONTEND_LOOK_Z = 1000.0,
+	FRONTEND_LOOK_Z = 800.0,
 
 	SETTINGS_POS_X = 790.0,
 	SETTINGS_POS_Y = 500.0,
