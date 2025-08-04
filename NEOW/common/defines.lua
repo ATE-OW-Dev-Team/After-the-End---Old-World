df7d1975dfda09340fdb5fb923e45f9e371d8773
Fishedotjpg <Fishe.jpg@gmail.com>
2025-08-04 23:28:31 +0100

    Update defines.lua

--- NEOW/common/defines.lua
+++ NEOW/common/defines.lua
@@ -1570,8 +1570,8 @@ NGraphics = {
 	SEVERE_WINTER_VALUE = 255,
 	TREASURY_MODIFIER_ICON_FRAME = 13,
 	NUMBER_OF_DNA_PROPERTIES = 11,
-	CITY_MAIN_BUILDING_LEVEL_2 = 25, -- If a main settlement has more building than that, it will use model level 2
-	CITY_MAIN_BUILDING_LEVEL_3 = 35, -- If a main settlement has more building than that, it will use model level 3
+	CITY_MAIN_BUILDING_LEVEL_2 = 92,											-- If a main settlement has more building than that, it will use model level 2
+	CITY_MAIN_BUILDING_LEVEL_3 = 93,											-- If a main settlement has more building than that, it will use model level 3
 	NUDGE_WONDER_MODEL_LAND = "wonder_generic",
 	NUDGE_WONDER_MODEL_COAST = "wonder_grand_lighthouse",
 },
